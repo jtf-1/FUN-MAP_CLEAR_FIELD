05f6c56043329d620b7359c2b26b2a4dff8c2b3f
gannon145 <matthewtwayne@gmail.com>
2022-04-20 23:07:36 -0400

    Complete Map Rework

---
+++ PROJECT_RedIADS.lua
@@ -0,0 +1,26 @@
+env.info("RED IADS Loading", false)
+
+-----------------
+-- REDFOR IADS --
+-----------------
+redIADS = SkynetIADS:create('Red IADS')
+redIADS:setUpdateInterval(5)
+redIADS:addEarlyWarningRadarsByPrefix('rEWR')
+redIADS:addSAMSitesByPrefix('rsam')
+redIADS:getSAMSitesByNatoName('SA-2'):setGoLiveRangeInPercent(80)
+redIADS:getSAMSitesByNatoName('SA-3'):setGoLiveRangeInPercent(80)
+redIADS:getSAMSitesByNatoName('SA-5'):setGoLiveRangeInPercent(80)
+redIADS:getSAMSitesByNatoName('SA-6'):setGoLiveRangeInPercent(80)
+redIADS:getSAMSitesByNatoName('SA-8'):setGoLiveRangeInPercent(80)
+redIADS:getSAMSitesByNatoName('SA-10'):setGoLiveRangeInPercent(80):setActAsEW(true)
+redIADS:getSAMSitesByNatoName('SA-11'):setGoLiveRangeInPercent(80)
+redIADS:getSAMSitesByNatoName('SA-13'):setGoLiveRangeInPercent(80)
+redIADS:getSAMSitesByNatoName('SA-15'):setGoLiveRangeInPercent(100)
+redIADS:getSAMSitesByNatoName('SA-19'):setGoLiveRangeInPercent(100)
+
+redIADS:addRadioMenu()  
+
+redIADS:activate()  
+  
+env.info("RED IADS Complete", false)
+
